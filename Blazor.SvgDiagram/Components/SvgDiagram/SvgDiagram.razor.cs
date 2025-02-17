32c18182e71c7ccb29cadb4c5edc8dec211c118b
Kate Kotova <katekotova_86@mail.ru>
2025-02-17 09:08:17 +0300

    Add the svg diagram example

---
+++ Blazor.SvgDiagram/Components/SvgDiagram/SvgDiagram.razor.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace Blazor.SvgDiagram.Components.SvgDiagram;
+
+public partial class SvgDiagram : IAsyncDisposable
+{
+    [Inject]
+    public IJSRuntime JSRuntime { get; set; }
+
+    [Parameter]
+    public string DiagramSvgId { get; set; } = "diagram";
+
+    private IJSObjectReference? _jsModule;
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            _jsModule = await JSRuntime.InvokeAsync<IJSObjectReference>("import",
+                "./js/svg-diagram/svg-diagram.js");
+
+            await CreateDiagramAsync();
+        }
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        if (_jsModule is not null)
+        {
+            try
+            {
+                await _jsModule.DisposeAsync();
+            }
+            catch (JSDisconnectedException)
+            {
+                // TODO: show an error message
+            }
+        }
+    }
+
+    public async Task CreateDiagramAsync()
+    {
+        if (_jsModule is not null)
+        {
+            await _jsModule.InvokeVoidAsync("createDiagram", DiagramSvgId);
+        }
+    }
+}
---
+++ Blazor.SvgDiagram/wwwroot/js/svg-diagram/svg-diagram.js
@@ -0,0 +1,4 @@
+﻿export function createDiagram(diagramId) {
+    var diagram = SVG(`#${diagramId}`).size(300, 300);
+    diagram.rect(100, 100).move(100, 50).fill('#f06');
+}
